77cad489cf7a28f199f4821292a6913a88c2ade8
Ryan Johnson <ryan.txanson@gmail.com>
2014-01-10 19:52:19 +0000

    colorize missing/found and add error message if FSTs are missing

--- src/neahtta/manage.py
+++ src/neahtta/manage.py
@@ -5,6 +5,8 @@ from flask import Flask
 from flaskext.actions import Manager
 from neahtta import app
 
+from fabric.colors import red, green
+
 manager = Manager(app, default_server_actions=True)
 
 @manager.register('compilemessages')
@@ -39,25 +41,26 @@ def hello(app):
         fsts = app.config.yaml.get('Morphology').iteritems()
         print ''
         print 'Checking config files and whether they exist...'
+        missing_fst = False
         for k, v in fsts:
             file_path = ''.join(v.get('file'))
             i_file_path = ''.join(v.get('inverse_file'))
-            file_exists   = 'MISSING: '
-            i_file_exists = 'MISSING: '
+            file_exists   = red('MISSING: ')
+            i_file_exists = red('MISSING: ')
             dates = 'UPDATED: ?'
             i_dates = 'UPDATED: ?'
             try:
                 with open(file_path):
-                    file_exists = 'FOUND:   '
+                    file_exists = green('FOUND:   ')
                     dates       = 'UPDATED: %s' % get_dates(file_path)
             except IOError:
-                pass
+                missing_fst = True
             try:
                 with open(i_file_path):
-                    i_file_exists = 'FOUND:   '
+                    i_file_exists = green('FOUND:   ')
                     i_dates       = 'UPDATED: %s' % get_dates(i_file_path)
             except IOError:
-                pass
+                missing_fst = True
 
             print "%s:" % k
             print "  " + file_exists + file_path
@@ -68,6 +71,8 @@ def hello(app):
             print ''
             print ''
 
+        if missing_fst:
+            print red("Some FSTs were not found. See above.")
         return False
     return action
 
