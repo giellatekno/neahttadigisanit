cb019a50cfad4d5e28db058daf3300ec4514cb92
Ryan Johnson <ryan.txanson@gmail.com>
2014-11-03 23:15:33 +0000

    shared scope, only run for visible elements

--- src/neahtta/static/js/async_paradigms.js
+++ src/neahtta/static/js/async_paradigms.js
@@ -12,7 +12,8 @@ var NDS = angular.module('NDS', []).
 
 NDS.directive('wordParadigm', function() {
     return {
-        restrict: 'A',
+        restrict: 'A', // Attribute name word-paradigm
+        scope: true,
         controller: function ($scope, $http, $element, $attrs) {
             var lem = $attrs.lemma;
 
@@ -28,38 +29,60 @@ NDS.directive('wordParadigm', function() {
             $element.addClass('loading');
             $scope.complete = false;
             $scope.paradigm = false;
-            setTimeout(function(){
-                $element.addClass('loading');
-                $http({url: paradigm_url, method: "GET", params: get_attrs})
-                    .success(function(data){ 
-                        $element.removeClass('loading');
-                        $element.find('.loading_spinner').remove();
 
-                        window.thanks_android = setInterval( function (){
-                            if ($scope.paradigm) {
-                                $element.removeClass('loading');
-                                $element.find('.loading_spinner').remove();
-                                clearInterval(window.thanks_android);
-                                delete window.thanks_android;
-                            }
-                        }, 100);
+            // define this as a function because we'll use it as an
+            // event too
 
-                        $scope.complete = true;
+            function run_request () {
+                setTimeout(function(){
+                    $element.addClass('loading');
+                    $http({url: paradigm_url, method: "GET", params: get_attrs})
+                        .success(function(data){ 
+                            $element.removeClass('loading');
+                            $element.find('.loading_spinner').hide();
 
-                        if(data.paradigms.length > 0) {
-                            if(data.paradigms[0].length > 0) {
-                                $scope.paradigm = data.paradigms[0];
-                                $scope.no_paradigm = false;
+                            window.thanks_android = setInterval( function (){
+                                if ($scope.paradigm) {
+                                    $element.removeClass('loading');
+                                    $element.find('.loading_spinner').hide();
+                                    clearInterval(window.thanks_android);
+                                    delete window.thanks_android;
+                                }
+                            }, 100);
+
+                            $scope.complete = true;
+
+                            if(data.paradigms.length > 0) {
+                                if(data.paradigms[0].length > 0) {
+                                    $scope.paradigm = data.paradigms[0];
+                                    $scope.no_paradigm = false;
+                                } else {
+                                    $scope.paradigm = false;
+                                    $scope.no_paradigm = true;
+                                }
                             } else {
                                 $scope.paradigm = false;
                                 $scope.no_paradigm = true;
                             }
-                        } else {
-                            $scope.paradigm = false;
-                            $scope.no_paradigm = true;
-                        }
-                    })
-            }, 100);
+                        })
+                }, 100);
+            }
+
+
+            if($element.is(":visible")) {
+                run_request();
+            } else {
+                // If this needs to be responsive, this works, but be
+                // careful... 
+                //
+                // var visibility_check = setInterval(function(){
+                //     // Watch for the element to become visible
+                //     if ($element.is(":visible")) {
+                //         clearInterval(visibility_check);
+                //         run_request();
+                //     }
+                // }, 3000);
+            }
 
         } ,
     }
