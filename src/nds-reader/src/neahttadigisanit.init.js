e76647d49cb0129e490c4b71e6a6d5fcf0e80511
Ryan Johnson <ryan.txanson@gmail.com>
2015-03-16 20:11:08 +0000

    resurrect the init.js. Hyvää Urhon päivää

---
+++ src/nds-reader/src/neahttadigisanit.init.js
@@ -0,0 +1,7 @@
+﻿jQuery(document).ready(function (){
+	// For some reason rangy needs to be initialized, but apparently only for
+	// the bookmarklet.
+    jQuery(document).selectToLookup({
+        api_host: NDS_API_HOST + '/',
+    });
+});
