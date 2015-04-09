150fea7bca36db1b27edfae0b9390406688de44e
Ryan Johnson <ryan.txanson@gmail.com>
2015-04-09 22:41:26 +0000

    helper script for switching directories

---
+++ src/neahtta/svn_switch_to_nds-stable-gtweb.sh
@@ -0,0 +1,5 @@
+﻿svn switch https://gtsvn.uit.no/langtech/tags/apps/dicts/nds/nds-stable-gtweb/src/neahtta .
+svn switch https://gtsvn.uit.no/langtech/trunk/apps/dicts/nds/src/neahtta/configs configs
+svn switch https://gtsvn.uit.no/langtech/trunk/apps/dicts/nds/src/neahtta/translations translations
+svn switch https://gtsvn.uit.no/langtech/trunk/apps/dicts/nds/src/neahtta/dicts dicts
+svn switch https://gtsvn.uit.no/langtech/trunk/apps/dicts/nds/src/neahtta/static static
