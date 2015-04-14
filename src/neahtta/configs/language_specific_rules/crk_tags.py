b4587b638ba8bb132a7041beaf5b17a3d6cb9be9
Ryan Johnson <ryan.txanson@gmail.com>
2015-04-14 21:24:29 +0000

    move and finish the function

--- src/neahtta/configs/language_specific_rules/crk_tags.py
+++ src/neahtta/configs/language_specific_rules/crk_tags.py
@@ -1,24 +1,27 @@
 import os, sys
 
 # TODO: replace PV/ with user-defined regex or something
-
-def process_crk_tag(analysis_string, tag_separator='+'):
-    """ Take an analysis string, and return a tuple of the lemma,
+def process_crk_analysis(analysis_line):
+    """ Take an analysis line, and return a tuple of the lemma,
     followed by a reformatted tag where tags before the lemma are
     combined with tags after the lemma. Strings without any preverb
     material are not changed.
 
-        >>> process_crk_tag("PV/asdf+PV/bbq+lemma+POS+Type+Sg1")
-        ('lemma', 'PV/asdf+PV/bbq+POS+Type+Sg1')
-        >>> process_crk_tag("lemma+POS+Type+Sg1")
-        ('lemma', 'lemma+POS+Type+Sg1')
-        >>> process_crk_tag("PV/asdf+PV/bbq+lemma")
-        ('lemma', 'PV/asdf+PV/bbq+POS+Type+Sg1')
+        >>> process_crk_analysis("wordform\tPV/asdf+PV/bbq+lemma+POS+Type+Sg1")
+        ('wordform', 'lemma+PV/asdf+PV/bbq+POS+Type+Sg1')
+        >>> process_crk_analysis("wordform\tlemma+POS+Type+Sg1")
+        ('wordform', 'lemma+POS+Type+Sg1')
+        >>> process_crk_analysis("wordform\tPV/asdf+PV/bbq+lemma")
+        ('wordform', 'lemma+PV/asdf+PV/bbq')
 
     """
+
+    wordform, _, analysis_string = analysis_line.partition('\t')
+
     lemma = False
 
-    tag_sep = tag_separator
+    tag_sep = '+'
+
     parts = analysis_string.split(tag_sep)
     has_preverbs = False
 
@@ -42,7 +45,7 @@ def process_crk_tag(analysis_string, tag_separator='+'):
             _lem, _, tag = analysis_string.partition(lemma + tag_sep)
             preverbs = None
 
-    reformatted_tag_parts = []
+    reformatted_tag_parts = [lemma]
 
     if preverbs:
         reformatted_tag_parts.append(preverbs)
@@ -51,15 +54,15 @@ def process_crk_tag(analysis_string, tag_separator='+'):
 
     reformatted_tag = tag_sep.join(reformatted_tag_parts)
 
-    return (lemma, reformatted_tag)
+    return (wordform, reformatted_tag)
 
 def main():
     print '--'
-    print process_crk_tag("PV/asdf+PV/bbq+lemma+POS+Type+Sg1")
+    print process_crk_analysis("PV/asdf+PV/bbq+lemma+POS+Type+Sg1")
     # ('lemma', 'PV/asdf+PV/bbq+POS+Type+Sg1')
-    print process_crk_tag("lemma+POS+Type+Sg1")
+    print process_crk_analysis("lemma+POS+Type+Sg1")
     # ('lemma', 'lemma+POS+Type+Sg1')
-    print process_crk_tag("PV/asdf+PV/bbq+lemma")
+    print process_crk_analysis("PV/asdf+PV/bbq+lemma")
 
 if __name__ == "__main__":
     sys.exit(main())
