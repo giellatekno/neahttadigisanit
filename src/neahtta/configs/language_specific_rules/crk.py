c07d792e49c4468057be6d988644fc475dd01f21
Ryan Johnson <ryan.txanson@gmail.com>
2015-08-07 17:29:31 +0000

    preprocessing for generating canonical forms, move tags that count as pre-lemma things before the tag in order

--- src/neahtta/configs/language_specific_rules/crk.py
+++ src/neahtta/configs/language_specific_rules/crk.py
@@ -1,20 +1,70 @@
-﻿## from lexicon import lexicon_overrides as lexicon
-## 
-## @lexicon.entry_target_formatter(('crk', 'eng'))
-## def format_target_sme(ui_lang, e, tg):
-##     """**Entry target translation formatter**
-## 
-##     Display @src (source) attribute in translations
-## 
-##     """
-##     _str_norm = 'string(normalize-space(%s))'
-## 
-##     _src = e.xpath(_str_norm % '@src')
-## 
-##     _t_lemma = tg.xpath(_str_norm % 't/text()')
-## 
-##     if _src:
-##         return "(%s) %s" % (src, _reg)
-## 
-##     return None
-## 
+﻿from morphology import generation_overrides as morphology
+
+@morphology.tag_filter_for_iso('crk')
+def adjust_tags_for_gen(lemma, tags, node=None, **kwargs):
+    """ **tag filter**: Lexicon -> FST changes.
+
+    Change POS to be compatible with FST for when they are not.
+    """
+
+    from flask import current_app, g
+    import re
+    # get tagset for pre-lemma stuff
+
+    morph = current_app.config.morphologies.get(g._from, False)
+
+    tagsets = morph.tagsets.sets
+
+    prelemmas = tagsets.get('prelemma_tags')
+    # TODO: where is the lemma
+
+    # print g._from
+    # print lemma
+    # print list(prelemmas.members)
+
+    cleaned_tags = []
+    for t in tags:
+        # print t
+
+        cleaned_tag = []
+
+        for pl in prelemmas.members:
+            before = []
+            rest = []
+
+            pl = unicode(pl)
+
+            try:
+                _pl = re.compile(pl)
+            except Exception, e:
+                _pl = False
+
+            for part in t:
+                if _pl:
+                    if _pl.match(part) or pl == part:
+                        before.append(part)
+                        continue
+                else:
+                    if pl == part:
+                        before.append(part)
+                        continue
+                rest.append(part)
+
+        # print before, rest
+
+        cleaned_tag.extend(before)
+        cleaned_tag.append(lemma)
+        cleaned_tag.extend(rest)
+
+        # print cleaned_tag
+
+        cleaned_tags.append(cleaned_tag)
+
+
+    if len(cleaned_tags) == 0 and len(tags) > 0:
+        tags = cleaned_tags
+
+    # print cleaned_tags
+
+    return lemma, cleaned_tags, node
+
