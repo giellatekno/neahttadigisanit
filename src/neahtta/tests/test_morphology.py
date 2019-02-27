481fa81de55eb78a26708f58a71c0ad1142e3813
Børre Gaup <albbas@gmail.com>
2019-02-27 11:27:52 +0000

    Test the tagprocessor

---
+++ src/neahtta/tests/test_morphology.py
@@ -0,0 +1,40 @@
+# -*- encoding: utf-8 -*-
+"""Test classes from morphology/morphology.py"""
+from __future__ import absolute_import
+import unittest
+import os
+import sys
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+sys.path.insert(0, os.path.dirname(HERE))
+
+from parameterized import parameterized
+
+from neahtta import app
+from morphology.morphology import HFST, Lemma, Morphology, Tagsets
+
+class TestHFST(unittest.TestCase):
+    """Test the HFST class."""
+
+    def setUp(self):
+        """Set up common variables."""
+        self.hfst = HFST(
+            'hfst-lookup',
+            '/usr/share/giella/sme/analyser-dict-gt-desc.hfstol',
+            ifst_file='/usr/share/giella/sme/generator-dict-gt-norm.hfstol',
+            options={
+                'derivationMarker': '+Der',
+                'tagsep': '+',
+                'inverse_tagsep': '+',
+                'compoundBoundary': '+Cmp#'
+            })
+
+    def test_tagprocessor(self):
+        """Test the tagprocessor."""
+        self.assertEqual(
+            self.hfst.tag_processor('guolit\tguolli+N+Sg+Nom\t0,000000'),
+            ('guolit', 'guolli+N+Sg+Nom'))
+
+
+if __name__ == '__main__':
+    unittest.main()
