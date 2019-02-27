f6821f94735734c992d40d151b123e8d465a9d2f
Børre Gaup <albbas@gmail.com>
2019-02-27 11:29:47 +0000

    Use the fst tool from main yaml file

--- src/neahtta/tests/test_morphology.py
+++ src/neahtta/tests/test_morphology.py
@@ -14,27 +14,21 @@ from neahtta import app
 from morphology.morphology import HFST, Lemma, Morphology, Tagsets
 
 
+def fst_tool():
+    return app.config.morphologies['sme'].tool
+
+
 class TestHFST(unittest.TestCase):
     """Test the HFST class."""
 
     def setUp(self):
         """Set up common variables."""
-        self.hfst = HFST(
-            'hfst-lookup',
-            '/usr/share/giella/sme/analyser-dict-gt-desc.hfstol',
-            ifst_file='/usr/share/giella/sme/generator-dict-gt-norm.hfstol',
-            options={
-                'derivationMarker': '+Der',
-                'tagsep': '+',
-                'inverse_tagsep': '+',
-                'compoundBoundary': '+Cmp#'
-            })
         self.app_context = app.app_context()
 
     def test_tagprocessor(self):
         """Test the tagprocessor."""
         self.assertEqual(
-            self.hfst.tag_processor('guolit\tguolli+N+Sg+Nom\t0,000000'),
+            fst_tool().tag_processor('guolit\tguolli+N+Sg+Nom\t0,000000'),
             ('guolit', 'guolli+N+Sg+Nom'))
 
     def test_clean(self):
@@ -48,19 +42,19 @@ guollái\tguollái+A+Sg+Nom\t0,000000'''
             'guollái+A+Sg+Nom'
         ])]
 
-        self.assertEqual(self.hfst.clean(got), wanted)
+        self.assertEqual(fst_tool().clean(got), wanted)
 
     def test_lookup(self):
         """Test the analyser."""
         with self.app_context:
-            got = self.hfst.lookup([u'guollái', u'viežži'])
+            got = fst_tool().lookup(
+                [u'guollái', u'viežži'])
             wanted = [(u'guollái', [u'guolli+N+Sg+Ill', u'guollái+A+Sg+Nom']),
-                      (u'viežži', [
-                          u'viežžat+V+TV+Der/NomAg+N+Sg+Acc',
-                          u'viežžat+V+TV+Der/NomAg+N+Sg+Gen',
-                          u'viežžat+V+TV+Der/NomAg+N+Sg+Nom',
-                          u'viežžat+V+TV+Imprt+Du2', u'viežžat+V+TV+PrsPrc'
-                      ])]
+                      (u'viežži',
+                       [u'viežžat+V+TV+PrsPrc', u'viežžat+V+TV+Imprt+Du2',
+                        u'viežžat+V+TV+Der/NomAg+N+Sg+Gen',
+                        u'viežžat+V+TV+Der/NomAg+N+Sg+Acc',
+                        u'viežžat+V+TV+Der/NomAg+N+Sg+Nom'])]
 
             self.maxDiff = None
             self.assertEqual(got, wanted)
@@ -68,21 +62,21 @@ guollái\tguollái+A+Sg+Nom\t0,000000'''
     def test_inverse_lookup_by_string(self):
         """Test the generator that works on strings."""
         with self.app_context:
-            got = self.hfst.inverselookup_by_string('guolli+N+Pl+Nom')
+            got = fst_tool().inverselookup_by_string('guolli+N+Pl+Nom')
             wanted = [('guolli+N+Pl+Nom', ['guolit'])]
 
             self.assertEqual(got, wanted)
 
     def test_split_analysis(self):
         """Test the analysis line splitter."""
-        got = self.hfst.splitAnalysis('guolli+N+Sg+Loc')
+        got = fst_tool().splitAnalysis('guolli+N+Sg+Loc')
         wanted = ['guolli', 'N', 'Sg', 'Loc']
 
         self.assertEqual(got, wanted)
 
     def test_format_tags(self):
         """Test the tag formatter."""
-        got = self.hfst.formatTag(self.hfst.splitAnalysis('guolli+N+Sg+Loc'))
+        got = fst_tool().formatTag(fst_tool().splitAnalysis('guolli+N+Sg+Loc'))
         wanted = 'guolli+N+Sg+Loc'
 
         self.assertEqual(got, wanted)
@@ -100,7 +94,7 @@ guollái\tguollái+A+Sg+Nom\t0,000000'''
         with self.app_context:
             self.maxDiff = None
             self.assertEqual(
-                self.hfst.inverselookup(lemma, tags), wanted, msg=name)
+                fst_tool().inverselookup(lemma, tags), wanted, msg=name)
 
 
 if __name__ == '__main__':
