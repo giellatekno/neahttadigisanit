ffd17ffcb8cdad783c46bff756351ce5ded79d64
Børre Gaup <albbas@gmail.com>
2019-02-27 11:30:05 +0000

    Test Morphology.generation

--- src/neahtta/tests/test_morphology.py
+++ src/neahtta/tests/test_morphology.py
@@ -8,6 +8,7 @@ import sys
 HERE = os.path.abspath(os.path.dirname(__file__))
 sys.path.insert(0, os.path.dirname(HERE))
 
+from lxml import etree
 from parameterized import parameterized
 
 from neahtta import app
@@ -91,6 +92,48 @@ guollái\tguollái+A+Sg+Nom\t0,000000'''
             self.maxDiff = None
             self.assertEqual(
                 fst_tool().inverselookup(lemma, tags), wanted, msg=name)
+
+
+class TestMorphology(unittest.TestCase):
+    """Test the Morphology class."""
+
+    def test_generate(self):
+        """Test the generator."""
+        tagsets = '''guolli+N+Sg+Nom
+guolli+N+Sg+Acc
+guolli+N+Sg+Gen
+guolli+N+Sg+Ill
+guolli+N+Sg+Loc
+guolli+N+Sg+Com
+guolli+N+Ess
+guolli+N+Pl+Nom
+guolli+N+Pl+Acc
+guolli+N+Pl+Gen
+guolli+N+Pl+Ill
+guolli+N+Pl+Loc
+guolli+N+Pl+Com'''
+
+
+        args = ('guolli', tagsets, etree.Element('e'))
+        kwargs = {'no_preprocess_paradigm': True}
+
+        with app.app_context():
+            got = app.config.morphologies['sme'].generate(*args, **kwargs)
+            wanted = [([u'guolli', u'N', u'Sg', u'Nom'], [u'guolli']),
+                      ([u'guolli', u'N', u'Sg', u'Acc'], [u'guoli']),
+                      ([u'guolli', u'N', u'Sg', u'Gen'], [u'guole', u'guoli']),
+                      ([u'guolli', u'N', u'Sg', u'Ill'], [u'guollái']),
+                      ([u'guolli', u'N', u'Sg', u'Loc'], [u'guolis']),
+                      ([u'guolli', u'N', u'Sg', u'Com'], [u'guliin']),
+                      ([u'guolli', u'N', u'Ess'], [u'guollin']),
+                      ([u'guolli', u'N', u'Pl', u'Nom'], [u'guolit']),
+                      ([u'guolli', u'N', u'Pl', u'Acc'], [u'guliid']),
+                      ([u'guolli', u'N', u'Pl', u'Gen'], [u'guliid']),
+                      ([u'guolli', u'N', u'Pl', u'Ill'], [u'guliide']),
+                      ([u'guolli', u'N', u'Pl', u'Loc'], [u'guliin']),
+                      ([u'guolli', u'N', u'Pl', u'Com'], [u'guliiguin'])]
+
+            self.assertEqual(got, wanted)
 
 
 if __name__ == '__main__':
