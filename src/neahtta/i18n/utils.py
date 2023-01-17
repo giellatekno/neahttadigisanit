66011718d70df8c510ec99cf98c910e121e7c921
trondtynnol <tty001@uit.no>
2023-01-17 09:44:31 +0100

    In case the .dat files which babel needs are not copied into babel's locale-data folder, do so. Also removing unnecessary unicode() calls from python 2

--- src/neahtta/i18n/utils.py
+++ src/neahtta/i18n/utils.py
@@ -1,10 +1,8 @@
-﻿from __future__ import absolute_import
-from flask import current_app
-
-try:
-    unicode
-except NameError:
-    unicode = str
+﻿from flask import current_app
+import babel
+from babel.core import UnknownLocaleError
+import os
+import inspect
 
 def iso_filter(_iso):
     """ These things are sort of a temporary fix for some of the
@@ -13,12 +11,27 @@ def iso_filter(_iso):
     """
     return current_app.config.ISO_TRANSFORMS.get(_iso, _iso)
 
+def copy_custom_locales():
+    """If the custom locale data was not copied into babel before starting up, 
+    do it here"""
+    babel_dir = os.path.dirname(inspect.getfile(babel))
+    babel_locale_data_dir = babel_dir + "/locale-data/"
+
+    current_dir = os.path.dirname(os.path.realpath(__file__))
+    custom_locale_data_files = current_dir + "/../../localedata/*"
+
+    os.system(f"cp {custom_locale_data_files} {babel_locale_data_dir}")
+
 
 def get_locale():
     """ Always return the three character locales
     """
     from flask_babel import get_locale as get_
 
-    locale = iso_filter(unicode(get_()))
+    try:
+        locale = iso_filter(get_())
+    except UnknownLocaleError:
+        copy_custom_locales()
+        locale = iso_filter(get_())
 
     return locale
