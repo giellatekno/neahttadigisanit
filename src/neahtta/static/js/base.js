1f809e1f022ac2a20d044ee3a2a51c89999083c8
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-01-31 17:20:40 +0100

    add temporary downtime maintencen banner

--- src/neahtta/static/js/base.js
+++ src/neahtta/static/js/base.js
@@ -39,8 +39,78 @@ window.getViewport = function (elem) {
     return viewport;
 }
 
+// anders: temporary maintainance notice
+var tnn_current_lang = "en";
+var tnn_notice_ru = "извещение";
+var tnn_notice_en = "Notice";
+var tnn_message_en = "Neahttadigisanit will be unavailable on the evening of DATE.";
+var tnn_message_ru = "Neahttadigisanit будет недоступен вечером DATE.";
+var tnn_in_english = "English";
+var tnn_in_russian = "на русском";
+// https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API
+function tnn_storageAvailable(type) {
+  var storage;
+  try {
+    storage = window[type];
+    const x = "__storage_test__";
+    storage.setItem(x, x);
+    storage.removeItem(x);
+    return true;
+  } catch (e) {
+    return (
+      e instanceof DOMException &&
+      // everything except Firefox
+      (e.code === 22 ||
+        // Firefox
+        e.code === 1014 ||
+        // test name field too, because code might not be present
+        // everything except Firefox
+        e.name === "QuotaExceededError" ||
+        // Firefox
+        e.name === "NS_ERROR_DOM_QUOTA_REACHED") &&
+      // acknowledge QuotaExceededError only if there's something already stored
+      storage &&
+      storage.length !== 0
+    );
+  }
+}
+function tnn_close_notice() {
+    var el = document.getElementById("tnn-maintainance-notice");
+    document.body.removeChild(el);
+
+    if (tnn_storageAvailable("sessionStorage")) {
+        window.sessionStorage.setItem("tnn-notice-seen", "yes");
+    }
+}
+function tnn_change_lang() {
+    var msg = document.getElementById("tnn-notice-msg");
+    var notice = document.getElementById("tnn-notice-notice");
+    var in_lang = document.getElementById("tnn-notice-changelang-btn");
+
+    if (tnn_current_lang == "en") {
+        tnn_current_lang = "ru";
+        msg.innerText = tnn_message_ru;
+        notice.innerText = tnn_notice_ru;
+        in_lang.innerText = tnn_in_english;
+    } else {
+        tnn_current_lang = "en";
+        msg.innerText = tnn_message_en;
+        notice.innerText = tnn_notice_en;
+        in_lang.innerText = tnn_in_russian;
+    }
+}
+// anders: END temporary maintainance notice
+
 var _im_listening = false;
 $(document).ready(function(){
+    // anders: temporary maintainance notice
+    if (tnn_storageAvailable("sessionStorage")) {
+        var item = window.sessionStorage.getItem("tnn-notice-seen");
+        if (item !== null) {
+            tnn_close_notice();
+        }
+    }
+    // anders: END temporary maintainance notice
 
     $(window).resize(function(o){
         $('a.last').html(window.outerHeight);
