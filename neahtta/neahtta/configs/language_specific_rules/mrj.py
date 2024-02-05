161a1f5ba09cf174c3bf88eb37ce1956026d525c
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-02-05 19:50:10 +0100

    fix path for langspecific mrj script

--- neahtta/neahtta/configs/language_specific_rules/mrj.py
+++ neahtta/neahtta/configs/language_specific_rules/mrj.py
@@ -4,7 +4,10 @@ mrj-specific overrides.
 
 """
 
-from configs.language_specific_rules.common import match_homonymy_entries, remove_blank
-from morpholex import morpholex_overrides as morpholex
+from neahtta.configs.language_specific_rules.common import (
+    match_homonymy_entries,
+    remove_blank,
+)
+from neahtta.morpholex import morpholex_overrides as morpholex
 
 morpholex.post_morpho_lexicon_override("mrj")(match_homonymy_entries)
