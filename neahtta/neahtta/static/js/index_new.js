5ce6be60b1021a8a6063fb9026e04e42ef7ce721
Anders Lorentsen <anders.lorentsen@gmail.com>
2024-02-16 13:42:29 +0100

    ...remember to add new files, add index_new.js

---
+++ neahtta/neahtta/static/js/index_new.js
@@ -0,0 +1,7 @@
+import "./DSt.js";
+import "./bootstrap-collapse.js";
+import "./bootstrap-dropdown.js";
+import "./bootstrap-tooltip.js";
+import "./standalone-app.js";
+import "./autocomplete.js";
+
